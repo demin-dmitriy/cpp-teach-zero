cece31f96d8537f9adb6c6dd3d0a7716dc65bd29
Demin Dmitriy <demindf@gmail.com>
2018-11-25 20:23:34 +0300

    add first exercises

---
+++ algo/integer.h
@@ -0,0 +1,21 @@
+/*
+    Вычисляет минимум двух чисел.
+
+    3 == min_int(3, 7)
+    7 == min_int(8, 7)
+ */
+int min_int(int x, int y)
+{
+    /* FILL IN */
+}
+
+/*
+    Вычисляет максимум двух чисел.
+
+    7 == max_int(3, 7)
+    8 == max_int(8, 7)
+*/
+int max_int(int x, int y)
+{
+    /* FILL IN */
+}
